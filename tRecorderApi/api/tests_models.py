2ff9e60026ebfa11c1f9425a41d06872eb40f79c
Pinto18 <pinto18.nd@gmail.com>
2017-06-30 10:18:24 -0400

    Added Test Cases For Delete Requests

--- tRecorderApi/api/tests_models.py
+++ tRecorderApi/api/tests_models.py
@@ -21,7 +21,7 @@ class ModelTestCase(TestCase):
         self.book = Book(code='en-demo',name='english', booknum=5)
 
     def test_model_can_create_a_take(self):
-        """Test the File model can create a file."""
+        """Test the File model can create a take."""
         old_count = Take.objects.count()  #obtain current count of object in database
         self.take.save()   #save object to database
         new_count = Take.objects.count()  #obtain new count of object in database
@@ -30,20 +30,19 @@ class ModelTestCase(TestCase):
         test_log.write("TEST: Creating and Storing a Take Object........................PASSED\n")
         test_log.close()
 
-    def test_model_can_create_a_Project(self):
-        """Test the Project model can create a file."""
+    def test_model_can_create_a_Language(self):
+        """Test the Language model can create a language."""
         old_count = Language.objects.count()
         self.language.save()
         new_count = Language.objects.count()
         self.assertNotEqual(old_count, new_count)
         test_log = open("test_log.txt", "a")
-        test_log.write("TEST: Creating and Storing a Project Object.....................PASSED\n")
+        test_log.write("TEST: Creating and Storing a Language Object....................PASSED\n")
         test_log.close()
 
     def test_model_can_create_a_User(self):
-        """Test the User model can create a file."""
+        """Test the User model can create a user."""
         old_count = User.objects.count()
-
         self.user.save()
         new_count = User.objects.count()
         self.assertNotEqual(old_count, new_count)
@@ -52,13 +51,23 @@ class ModelTestCase(TestCase):
         test_log.close()
 
     def test_model_can_create_a_Comment(self):
-        """Test the Comment model can create a file."""
+        """Test the Comment model can create a comment."""
         old_count = Comment.objects.count()
         self.comment.save()
         new_count = Comment.objects.count()
         self.assertNotEqual(old_count, new_count)
         test_log = open("test_log.txt", "a")
         test_log.write("TEST: Creating and Storing a Comment Object.....................PASSED\n")
+        test_log.close()
+
+    def test_model_can_create_a_Book(self):
+        """Test the Language model can create a language."""
+        old_count = Book.objects.count()
+        self.book.save()
+        new_count = Book.objects.count()
+        self.assertNotEqual(old_count, new_count)
+        test_log = open("test_log.txt", "a")
+        test_log.write("TEST: Creating and Storing a Language Object....................PASSED\n")
         test_log.close()
 
     def test_each_model_can_be_read_in_a_human_readable_format(self):
--- tRecorderApi/api/tests_views.py
+++ tRecorderApi/api/tests_views.py
@@ -20,10 +20,16 @@ class ViewTestCases(TestCase):
         self.lang_data = {'lang' : 'english', 'code' : 'abc'}
         self.user_data = {'name' : 'tester', 'agreed' : True, 'picture' : 'test.pic'}
         self.comment = {'location':'test_location'}
+        self.book_data = {'code':'ex', 'name' : 'english', 'booknum' : 5}
+        self.take_object = Take(location='test_location', id=1)
+        self.language_object = Language(code='en-demo', name='english', id=1)
+        self.book_object = Book(code='en-demo', name='english', booknum=5, id=1)
+        self.user_object = User(name='testy', agreed=True, picture='mypic.jpg', id=1)
+        self.comment_object = Comment.objects.create(location='/test-location/', id=1)
 
     def test_api_can_create_take_object(self):
         """Test the API has take creation capability:
-        Sending JSON File Object To API and
+        Sending JSON Take Object To API and
         Expecting HTTP Success Message Returned"""
         self.response = self.client.post(base_url + 'takes/', self.take_data, format='json') #send POST to API
         self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
@@ -33,7 +39,7 @@ class ViewTestCases(TestCase):
 
     def test_api_can_create_lang_object(self):
         """Test the API has lang creation capability:
-        Sending JSON File Object To API and
+        Sending JSON Lang Object To API and
         Expecting HTTP Success Message Returned"""
         self.response = self.client.post(base_url + 'languages/', self.lang_data,format='json')  # send POST to API
         self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
@@ -41,9 +47,19 @@ class ViewTestCases(TestCase):
         test_log.write("TEST: Posting Language Object to API............................PASSED\n")
         test_log.close()
 
+    def test_api_can_create_book_object(self):
+        """Test the API has book creation capability:
+        Sending JSON Book Object To API and
+        Expecting HTTP Success Message Returned"""
+        self.response = self.client.post(base_url + 'books/', self.book_data,format='json')  # send POST to API
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+        test_log = open("test_log.txt", "a")
+        test_log.write("TEST: Posting Book Object to API................................PASSED\n")
+        test_log.close()
+
     def test_api_can_create_comment_object(self):
-        """Test the API has lang creation capability:
-        Sending JSON File Object To API and
+        """Test the API has comment creation capability:
+        Sending JSON Comment Object To API and
         Expecting HTTP Success Message Returned"""
         self.response = self.client.post(base_url + 'comments/', self.comment,format='json')  # send POST to API
         self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
@@ -52,6 +68,9 @@ class ViewTestCases(TestCase):
         test_log.close()
 
     def test_that_api_can_create_user_object(self):
+        """Test the API has user creation capability:
+        Sending JSON User Object To API and
+        Expecting HTTP Success Message Returned"""
         self.response = self.client.post(base_url + 'users/', self.user_data, format='json')
         self.assertEquals(self.response.status_code, status.HTTP_201_CREATED)
         test_log = open("test_log.txt", "a")
@@ -62,10 +81,10 @@ class ViewTestCases(TestCase):
         """Test that the API can update a take object:
         Sending Take Object To API and
         Expecting HTTP Success Message Returned"""
-        take_object = Take.objects.create(location='test_location', id=1)
+        self.take_object.save()
         response = self.client.put(base_url + 'takes/1/', {'location' : 'new_location'}, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        take_object.delete()
+        self.take_object.delete()
         self.assertEqual(0, len(Take.objects.filter(id=1)))
         test_log = open("test_log.txt", "a")
         test_log.write("TEST: Sending PUT Request For Take Object To API................PASSED\n")
@@ -75,10 +94,10 @@ class ViewTestCases(TestCase):
         """Test that the API can update a language object:
         Sending Language Object To API and
         Expecting HTTP Success Message Returned"""
-        language_object = Language.objects.create(code='en-demo',name='english', id=1)
+        self.language_object.save()
         response = self.client.put(base_url + 'languages/1/', {'code' : 'ex-demo'}, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        language_object.delete()
+        self.language_object.delete()
         self.assertEqual(0, len(Language.objects.filter(id=1)))
         test_log = open("test_log.txt", "a")
         test_log.write("TEST: Sending PUT Request For Language Object To API............PASSED\n")
@@ -88,10 +107,10 @@ class ViewTestCases(TestCase):
         """Test that the API can update a book object:
         Sending Book Object To API and
         Expecting HTTP Success Message Returned"""
-        book_object = Book.objects.create(code='en-demo', name='english', booknum=5, id=1)
+        self.book_object.save()
         response = self.client.put(base_url + 'books/1/', {'name': 'spanish'}, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        book_object.delete()  # delete object from temporary database
+        self.book_object.delete()  # delete object from temporary database
         self.assertEqual(0, len(Book.objects.filter(id=1)))
         test_log = open("test_log.txt", "a")
         test_log.write("TEST: Sending PUT Request For Book Object To API................PASSED\n")
@@ -101,11 +120,10 @@ class ViewTestCases(TestCase):
         """Test that the API can update a User object:
         Sending User Object To API and
         Expecting HTTP Success Message Returned"""
-        user_object = User.objects.create(name='testy', agreed=True, picture='mypic.jpg', id=1)
-        user_object.name = 'tester'
+        self.user_object.save()
         response = self.client.put(base_url + 'users/1/', {'name' : 'nick', 'picture' : 'newpic.jpg'}, format='json') #picture is required to change when updating user object
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        user_object.delete()  # delete object from temporary database
+        self.user_object.delete()  # delete object from temporary database
         self.assertEqual(0, len(User.objects.filter(id=1)))
         test_log = open("test_log.txt", "a")
         test_log.write("TEST: Sending PUT Request For User Object To API................PASSED\n")
@@ -115,10 +133,10 @@ class ViewTestCases(TestCase):
         """Test that the API can update a User object:
         Sending User Object To API and
         Expecting HTTP Success Message Returned"""
-        comment_object = Comment.objects.create(location='/test-location/', id=1)
+        self.comment_object.save()
         response = self.client.put(base_url + 'comments/1/', {'location' : '/new-location/'}, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        comment_object.delete()  # delete object from temporary database
+        self.comment_object.delete()  # delete object from temporary database
         self.assertEqual(0, len(Comment.objects.filter(id=1)))
         test_log = open("test_log.txt", "a")
         test_log.write("TEST: Sending PUT Request For Comment Object To API.............PASSED\n")
@@ -126,10 +144,10 @@ class ViewTestCases(TestCase):
 
     def test_get_take_request_returns_success(self):
          """Testing API can handle GET requests for Take objects"""
-         take_object = Take.objects.create(location='test_location', id=1)
+         self.take_object.save()
          response = self.client.get(base_url + 'takes/1/')
          self.assertEqual(response.status_code, status.HTTP_200_OK)
-         take_object.delete()    #delete object from temporary database
+         self.take_object.delete()    #delete object from temporary database
          self.assertEqual(0,len(Take.objects.filter(id=1)))  #check that take_object was deleted from temporary database
          test_log = open("test_log.txt", "a")
          test_log.write("TEST: Sending GET request for Take Object to API................PASSED\n")
@@ -137,10 +155,10 @@ class ViewTestCases(TestCase):
 
     def test_get_language_request_returns_success(self):
          """Testing API can handle GET requests for Language objects"""
-         language_object = Language.objects.create(code='en-demo',name='english', id=1)
+         self.language_object.save()
          response = self.client.get(base_url + 'languages/1/')
          self.assertEqual(response.status_code, status.HTTP_200_OK)
-         language_object.delete()    #delete object from temporary database
+         self.language_object.delete()    #delete object from temporary database
          self.assertEqual(0,len(Language.objects.filter(id=1)))  #check that object was deleted from temporary database
          test_log = open("test_log.txt", "a")
          test_log.write("TEST: Sending GET request for Language Object to API............PASSED\n")
@@ -148,10 +166,10 @@ class ViewTestCases(TestCase):
 
     def test_get_book_request_returns_success(self):
          """Testing API can handle GET requests for Book objects"""
-         book_object = Book.objects.create(code='en-demo',name='english', booknum=5, id=1)
+         self.book_object.save()
          response = self.client.get(base_url + 'books/1/')
          self.assertEqual(response.status_code, status.HTTP_200_OK)
-         book_object.delete()    #delete object from temporary database
+         self.book_object.delete()    #delete object from temporary database
          self.assertEqual(0,len(Book.objects.filter(id=1)))  #check that object was deleted from temporary database
          test_log = open("test_log.txt", "a")
          test_log.write("TEST: Sending GET request for Book Object to API................PASSED\n")
@@ -159,10 +177,10 @@ class ViewTestCases(TestCase):
 
     def test_get_user_request_returns_success(self):
          """Testing API can handle GET requests for User objects"""
-         user_object = User.objects.create(name='testy',agreed=True, picture='mypic.jpg', id=1)
+         self.user_object.save()
          response = self.client.get(base_url + 'users/1/')
          self.assertEqual(response.status_code, status.HTTP_200_OK)
-         user_object.delete()    #delete object from temporary database
+         self.user_object.delete()    #delete object from temporary database
          self.assertEqual(0,len(User.objects.filter(id=1)))  #check that object was deleted from temporary database
          test_log = open("test_log.txt", "a")
          test_log.write("TEST: Sending GET request for User Object to API................PASSED\n")
@@ -170,14 +188,59 @@ class ViewTestCases(TestCase):
 
     def test_get_comment_request_returns_success(self):
          """Testing API can handle GET requests for Comment objects"""
-         comment_object = Comment.objects.create(location='/test-location/', id=1)
+         self.comment_object.save()
          response = self.client.get(base_url + 'comments/1/')
          self.assertEqual(response.status_code, status.HTTP_200_OK)
-         comment_object.delete()    #delete object from temporary database
+         self.comment_object.delete()    #delete object from temporary database
          self.assertEqual(0,len(Comment.objects.filter(id=1)))  #check that object was deleted from temporary database
          test_log = open("test_log.txt", "a")
          test_log.write("TEST: Sending GET request for Comment Object to API.............PASSED\n")
          test_log.close()
+
+    def test_that_api_can_delete_take_objects(self):
+        """Testing that the API has Take Object deletion functionality"""
+        self.take_object.save()
+        response = self.client.delete(base_url + 'takes/1/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT) #after deleting an object, nothing should be returned, which is why we check against a 204 status code
+        test_log = open("test_log.txt", "a")
+        test_log.write("TEST: Sending DELETE request for Comment Object to API...........PASSED\n")
+        test_log.close()
+
+    def test_that_api_can_delete_language_objects(self):
+        """Testing that the API has Language Object deletion functionality"""
+        self.language_object.save()
+        response = self.client.delete(base_url + 'languages/1/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT) #after deleting an object, nothing should be returned, which is why we check against a 204 status code
+        test_log = open("test_log.txt", "a")
+        test_log.write("TEST: Sending DELETE request for Language Object to API..........PASSED\n")
+        test_log.close()
+
+    def test_that_api_can_delete_book_objects(self):
+        """Testing that the API has Take Object deletion functionality"""
+        self.book_object.save()
+        response = self.client.delete(base_url + 'books/1/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT) #after deleting an object, nothing should be returned, which is why we check against a 204 status code
+        test_log = open("test_log.txt", "a")
+        test_log.write("TEST: Sending DELETE request for Book Object to API..............PASSED\n")
+        test_log.close()
+
+    def test_that_api_can_delete_user_objects(self):
+        """Testing that the API has User Object deletion functionality"""
+        self.user_object.save()
+        response = self.client.delete(base_url + 'users/1/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT) #after deleting an object, nothing should be returned, which is why we check against a 204 status code
+        test_log = open("test_log.txt", "a")
+        test_log.write("TEST: Sending DELETE request for User Object to API..............PASSED\n")
+        test_log.close()
+
+    def test_that_api_can_delete_comment_objects(self):
+        """Testing that the API has Take Object deletion functionality"""
+        self.comment_object.save()
+        response = self.client.delete(base_url + 'comments/1/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT) #after deleting an object, nothing should be returned, which is why we check against a 204 status code
+        test_log = open("test_log.txt", "a")
+        test_log.write("TEST: Sending DELETE request for Comment Object to API...........PASSED\n")
+        test_log.close()
 
     def test_posting_file_to_api_returns_success_response(self):
         """Testing That zip files can be uploaded to the api"""
