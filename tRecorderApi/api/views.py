132609ce4c1b1bc9070ee2a617d5ad698b7849d2
dmaharjan <dilipmaharjann@gmail.com>
2017-07-10 14:38:12 -0400

    hard-coded directory separation changed

--- tRecorderApi/api/views.py
+++ tRecorderApi/api/views.py
@@ -1,48 +1,53 @@
-from django.http import HttpResponse, StreamingHttpResponse
-from django.shortcuts import render
-from django.core.files.storage import FileSystemStorage
+import hashlib
 import json
-from django.core import serializers
-import zipfile
-import urllib2
+import os
 import pickle
-#from os import remove
+import time
+import urllib2
+import uuid
+import zipfile
+
+import pydub
+from django.http import StreamingHttpResponse
+from django.shortcuts import render
+# from os import remove
 from rest_framework import viewsets, views, status
+from rest_framework.generics import ListAPIView
+from rest_framework.parsers import JSONParser, FileUploadParser, MultiPartParser, FormParser
 from rest_framework.response import Response
-from rest_framework.parsers import JSONParser, FileUploadParser
-from parsers import MP3StreamParser
+from rest_framework.views import APIView
+from tinytag import TinyTag
+
+from parsers import MP3StreamParser, CustomUploadParser
+from .models import Language, Book, User, Take, Comment
 from .serializers import LanguageSerializer, BookSerializer, UserSerializer
 from .serializers import TakeSerializer, CommentSerializer
-from .models import Language, Book, User, Take, Comment
-import pydub
-import time
-import uuid
-import os
-from tinytag import TinyTag
-import urllib2
-import pickle
+
 
 class LanguageViewSet(viewsets.ModelViewSet):
     """This class handles the http GET, PUT and DELETE requests."""
     queryset = Language.objects.all()
     serializer_class = LanguageSerializer
 
+
 class BookViewSet(viewsets.ModelViewSet):
     """This class handles the http GET, PUT and DELETE requests."""
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
 
 class UserViewSet(viewsets.ModelViewSet):
     """This class handles the http GET, PUT and DELETE requests."""
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+
 class TakeViewSet(viewsets.ModelViewSet):
     """This class handles the http GET, PUT and DELETE requests."""
     queryset = Take.objects.all()
     serializer_class = TakeSerializer
 
-    def destroy(self, request, pk = None):
+    def destroy(self, request, pk=None):
         instance = self.get_object()
         try:
             os.remove(instance.location)
@@ -50,13 +55,14 @@ class TakeViewSet(viewsets.ModelViewSet):
             pass
         self.perform_destroy(instance)
         return Response(status=status.HTTP_200_OK)
+
 
 class CommentViewSet(viewsets.ModelViewSet):
     """This class handles the http GET, PUT and DELETE requests."""
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
 
-    def destroy(self, request, pk = None):
+    def destroy(self, request, pk=None):
         instance = self.get_object()
         try:
             os.remove(instance.location)
@@ -64,6 +70,7 @@ class CommentViewSet(viewsets.ModelViewSet):
             pass
         self.perform_destroy(instance)
         return Response(status=status.HTTP_200_OK)
+
 
 class ProjectViewSet(views.APIView):
     parser_classes = (JSONParser,)
@@ -106,18 +113,22 @@ class ProjectViewSet(views.APIView):
 
         return Response(lst, status=200)
 
+
 class FileUploadView(views.APIView):
     parser_classes = (FileUploadParser,)
+
     def post(self, request, filename, format='zip'):
         if request.method == 'POST' and request.data['file']:
             uuid_name = str(time.time()) + str(uuid.uuid4())
             upload = request.data["file"]
-            #unzip files
+
+            # unzip files
             zip = zipfile.ZipFile(upload)
-            file_name = 'media/dump/' + uuid_name
+            file_name = "media" + os.sep + "dump" + os.sep + uuid_name
+
             zip.extractall(file_name)
             zip.close()
-            #extract metadata / get the apsolute path to the file to be stored
+            # extract metadata / get the apsolute path to the file to be stored
 
             # Cache language and book to re-use later
             bookname = ''
@@ -145,13 +156,11 @@ class FileUploadView(views.APIView):
                         "langname": langname,
                         "bookname": bookname,
                         "duration": meta.duration
-                        }
+                    }
                     prepareDataToSave(pls, abpath, data)
             return Response({"response": "ok"}, status=200)
         else:
             return Response(status=404)
-
-
 
 
 class FileStreamView(views.APIView):
@@ -163,39 +172,43 @@ class FileStreamView(views.APIView):
 
         return StreamingHttpResponse(file)
 
+
 def index(request):
     return render(request, 'index.html')
 
+
 def prepareDataToSave(meta, abpath, data):
     book, b_created = Book.objects.get_or_create(
-        code = meta["slug"],
+        code=meta["slug"],
         defaults={'code': meta['slug'], 'booknum': meta['book_number'], 'name': data['bookname']},
     )
     language, l_created = Language.objects.get_or_create(
-        code = meta["language"],
+        code=meta["language"],
         defaults={'code': meta['language'], 'name': data['langname']},
     )
     markers = convertstring(meta['markers'])
     # TODO get author of file and save it to Take model
     take = Take(location=abpath,
-                duration = data['duration'],
-                book = book,
-                language = language,
-                rating = 0, checked_level = 0,
-                anthology = meta['anthology'],
-                version = meta['version'],
-                mode = meta['mode'],
-                chapter = meta['chapter'],
-                startv = meta['startv'],
-                endv = meta['endv'],
-                markers = markers)
+                duration=data['duration'],
+                book=book,
+                language=language,
+                rating=0, checked_level=0,
+                anthology=meta['anthology'],
+                version=meta['version'],
+                mode=meta['mode'],
+                chapter=meta['chapter'],
+                startv=meta['startv'],
+                endv=meta['endv'],
+                markers=markers)
     take.save()
+
 
 def convertstring(dictionary):
     if not isinstance(dictionary, dict):
         return dictionary
     return dict((str(k), convertstring(v))
-        for k, v in dictionary.items())
+                for k, v in dictionary.items())
+
 
 def getLanguageByCode(code):
     url = 'http://td.unfoldingword.org/exports/langnames.json'
@@ -206,7 +219,7 @@ def getLanguageByCode(code):
         with open('language.json', 'wb') as fp:
             pickle.dump(languages, fp)
     except urllib2.URLError, e:
-        with open ('language.json', 'rb') as fp:
+        with open('language.json', 'rb') as fp:
             languages = pickle.load(fp)
 
     ln = ""
@@ -216,9 +229,10 @@ def getLanguageByCode(code):
             break
     return ln
 
+
 def getBookByCode(code):
-    with open('books.json') as books_file:    
-        books = json.load(books_file) 
+    with open('books.json') as books_file:
+        books = json.load(books_file)
 
     bn = ""
     for dicti in books:
