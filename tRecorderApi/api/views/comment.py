a49c051c1b42568c4661362aff73a9516a1628c0
mXaln <murmax82@gmail.com>
2017-07-12 13:07:17 -0400

    added source upload view

---
+++ tRecorderApi/api/views/comment.py
@@ -0,0 +1,19 @@
+from api.models import Comment
+from rest_framework import viewsets, status
+from api.serializers import CommentSerializer
+from rest_framework.response import Response
+import os
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """This class handles the http GET, PUT, PATCH, POST and DELETE requests."""
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+    def destroy(self, request, pk=None):
+        instance = self.get_object()
+        try:
+            os.remove(instance.location)
+        except OSError:
+            pass
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_200_OK)
