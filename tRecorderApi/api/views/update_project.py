ad950cf61da0971035dc3d3800f80251ae0e20fe
mXaln <murmax82@gmail.com>
2017-07-19 16:36:57 -0400

    improved update_ptoject endpoint

--- tRecorderApi/api/views/update_project.py
+++ tRecorderApi/api/views/update_project.py
@@ -12,11 +12,11 @@ class UpdateProjectView(views.APIView):
         data = request.data
 
         if "filter" not in data or "fields" not in data:
-            return Response({"response": "notenoughparameters"}, status=403)
+            return Response({"response": "not_enough_parameters"}, status=400)
 
-        result = updateTakesByProject(data)
+        try:
+            result = updateTakesByProject(data)
+        except Exception as e:
+            return Response({"error": str(e)}, status=400)
 
-        if type(result) is not int:
-            return Response({"error": str(result)}, status=400)
-        
-        return Response({"response": {"rows_affected": result}}, status=200)
+        return Response({"rows_affected": result}, status=200)
