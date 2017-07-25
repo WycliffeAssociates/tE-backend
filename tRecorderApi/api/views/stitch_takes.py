3666cbc4b45707f10b1a3e5d6834c9fc4531c5b1
lcheng49 <lukcheng@umich.edu>
2017-07-25 16:53:13 -0400

    Stitches propery but need to add naming scheme

--- tRecorderApi/api/views/stitch_takes.py
+++ tRecorderApi/api/views/stitch_takes.py
@@ -4,6 +4,8 @@ from rest_framework.parsers import JSONParser
 from api.models import Take, Language, Book, User, Comment, Project
 import json
 from rest_framework.response import Response
+import os
+
 
 
 class SourceStitchView(views.APIView):
@@ -11,6 +13,30 @@ class SourceStitchView(views.APIView):
     def post(self, request):
         data = request.data
         data["is_source"] = True
-        lst = Take.stitchSource(data)
+        if "language" in data and "version" in data and "book" in data and "chapter" in data:
+            lst = Take.stitchSource(data)
+            lst = list(lst)
+            lst = sorted(lst, key = lambda x: x["startv"])
+            loclst = []
+            for chunky in lst:
+                take = Take.objects.filter(chunk = chunky["id"]).values()
+                take = list(take)
+                loclst.append(take[0]["location"])
 
+            stitchedSource = AudioSegment.from_mp3(loclst[0])
+            loclst.pop(0)
+            print(type(stitchedSource))
+            for item in loclst:
+                sound1 = stitchedSource
+                sound2 = AudioSegment.from_mp3(item)
+                stitchedSource = sound1 + sound2
+            stitch_folder = 'media/source'
+            if not os.path.exists(stitch_folder):
+                os.makedirs(stitch_folder)
+            stitchedSource.export("./media/source/output.mp3", format="mp3")
+
+            return Response(loclst, status = 200)
+
+        else:
+            return Response({"error": "not_enough_parameters"}, status=400)
         return Response(lst, status = 200)
