3420f0baf0a9d3cb6e2ca63037e21c3d598541c1
dilipMaharjan <dilipmaharjann@gmail.com>
2017-10-24 16:20:08 -0400

    Revert "removed unnecessary constraint"
    This reverts commit cf5604fdd52c9e58d0968f735be037d1f0f275b1.

--- tRecorderApi/api/models/language.py
+++ tRecorderApi/api/models/language.py
@@ -1,8 +1,9 @@
 from django.db import models
 
+
 class Language(models.Model):
-    slug = models.CharField(unique=True)
-    name = models.CharField()
+    slug = models.CharField(unique=True, max_length=50)
+    name = models.CharField(max_length=255)
 
     @staticmethod
     def get_language(slug):
