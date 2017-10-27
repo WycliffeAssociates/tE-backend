bd108ae96f44b6ff62125dc0fde4efc8423bb5ba
dilipMaharjan <dilipmaharjann@gmail.com>
2017-10-27 10:06:08 -0400

    clean up

--- tRecorderApi/api/tests/tests_model_language.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-from api.models import Language
-
-class LanguageModelTests(TestCase):
-    
-    def test_gets_languages():
-        language=Language.get_language()
-        
