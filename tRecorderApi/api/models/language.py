eaa8dad4c02188cfa4a1e622dc176cbce907d02e
dilipMaharjan <dilipmaharjann@gmail.com>
2017-10-31 13:46:48 -0400

    language_view tested

--- tRecorderApi/api/models/language.py
+++ tRecorderApi/api/models/language.py
@@ -4,14 +4,6 @@ class Language(models.Model):
     
     slug = models.CharField(unique=True, max_length=50)
     name = models.CharField(max_length=255)
-    
-    def get_language(language_slug=None):
-        if(language_slug):
-            return Language.objects.filter(language_slug__iexact=language_slug)
-        return None
-
-    def get_languages():
-        return Language.objects.all()
 
     class Meta:
         ordering = ["name"]
--- tRecorderApi/api/urls.py
+++ tRecorderApi/api/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import url
 from rest_framework import routers
-import api.views
+from .views import language
 
 router = routers.DefaultRouter()
 # router.register(r'projects', views.ProjectViewSet)
 # router.register(r'chapters', views.ChapterViewSet)
 # router.register(r'chunks', views.ChunkViewSet)
-# router.register(r'languages', views.LanguageViewSet)
+#router.register(r'languages', language.LanguageViewSet)
 # router.register(r'books', views.BookViewSet)
 # router.register(r'users', views.UserViewSet)
 # router.register(r'takes', views.TakeViewSet)
@@ -24,11 +24,14 @@ urlpatterns = [
     # url(r'^exclude_files/$', views.ExcludeFilesView.as_view()),
     # url(r'^all_projects/$', views.AllProjectsView.as_view()),
     # url(r'^get_chapters/$', views.ProjectChapterInfoView.as_view()),
-    # url(r'^get_langs/$', views.getLangsView.as_view()),
+    url(r'^get_langs/$', language.GetLanguages.as_view()),
     # url(r'^get_versions/$', views.getVersionsView.as_view()),
     # url(r'^get_books/$', views.getBooksView.as_view()),
     # url(r'^push_takes/$', views.PushTakesView.as_view()),
     # url(r'^stitch_takes/$', views.SourceStitchView.as_view()),
+
+    # url(r'^languages/$',languageView.languages),
+    # url(r'^languages/(?P<id>\d+)/$', languageView.language),
 ]
 
 urlpatterns += router.urls
