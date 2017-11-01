052eb2d2cfc49f8789d734ee85b6a31e32c5b721
dilipMaharjan <dilipmaharjann@gmail.com>
2017-11-01 09:48:44 -0400

    clean up

--- tRecorderApi/api/models/anthology.py
+++ tRecorderApi/api/models/anthology.py
@@ -5,13 +5,5 @@ class Anthology(models.Model):
     slug = models.CharField(unique=True, max_length=50)
     name = models.CharField(max_length=255)
 
-    def get_anthology(anthology_slug=None):
-        if(anthology_slug):
-            return Anthology.objects.filter(slug__iexact=anthology_slug)
-        return None
-
-    def get_anthologies():
-        return Anthology.objects.all()
-
     def __str__(self):
         return self.name
