d6adc1bc7c352a250ddff7859046eaf250e7dc44
dilipMaharjan <dilipmaharjann@gmail.com>
2017-11-29 09:33:54 -0500

    api docs integrated

--- tRecorderApi/tRecorderApi/urls.py
+++ tRecorderApi/tRecorderApi/urls.py
@@ -15,10 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.conf import settings
-from django.conf.urls.static import static
+from rest_framework.documentation import include_docs_urls
+
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Pastebin API')
 
 urlpatterns = [
+    url(r'^docs/', include_docs_urls(title='tE Api ')),
+    url(r'^sdocs/', schema_view),
     url(r'^api/', include('api.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^auth/', include('rest_framework.urls', namespace='rest_framework'))
