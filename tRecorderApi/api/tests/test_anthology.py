2824f0ef9d47fc5fc00b3d4eded60cb5758f01a8
dilipMaharjan <dilipmaharjann@gmail.com>
2018-01-03 09:56:59 -0500

    few more test cases for anthology

--- tRecorderApi/api/tests/test_anthology.py
+++ tRecorderApi/api/tests/test_anthology.py
@@ -13,6 +13,11 @@ class AnthologyTest(TestCase):
         Anthology.objects.create(name='New Testament', slug='nt')
         Anthology.objects.create(name='Old Testament', slug='ot')
 
+    def test_number_of_items_are_equal(self):
+        anthology_num = Anthology.objects.count()
+        response = self.client.get('/api/anthologies/')
+        self.assertEqual(len(response.data), anthology_num)
+
     def test_get_request_has_200_status_code(self):
         response = self.client.get('/api/anthologies/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -28,3 +33,7 @@ class AnthologyTest(TestCase):
     def test_get_request_with_non_existent_id_has_404_status_code(self):
         response = self.client.get('/api/anthologies/4/')
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_random_text_as_parameter_gives_400_status_code(self):
+        response = self.client.get('/api/anthologies/?randomeparameter/')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
