7ff83403e793bafe8b0964fab458074c2ea5c45b
Joe <jsarabia1247@gmail.com>
2018-01-23 10:11:15 -0500

    Remove old view

--- tRecorderApi/api/views/exclude_files.py
+++ tRecorderApi/api/views/exclude_files.py
@@ -5,27 +5,6 @@ from .helpers import getFileName, md5Hash
 from rest_framework.response import Response
 from api.models import Take
 from api.serializers import ExcludeFilesSerializer
-
-
-class ExcludeFilesView(views.APIView):
-    parser_classes = (JSONParser,)
-
-    #The purpose of this request is to reduce the amount of unnecessary file uploads
-    #during backup from translationRecorder
-    def post(self, request):
-        files_to_exclude = {}
-        data = request.data
-        #get the files associated with a particular project
-        project = Take.objects.filter()
-        for chunk in project["chunks"]:
-            for take in chunk['takes']:
-                location = take['take']['location']
-                #for all files in the project, compute the md5 hash
-                files_to_exclude[getFileName(location)] = md5Hash(location)
-        #return the md5 hashes of files we have, so that duplicates can be avoided
-        return Response(files_to_exclude, status=200)
-
-    from api.models import Take
 from rest_framework import viewsets
 from api.serializers import TakeSerializer
 
