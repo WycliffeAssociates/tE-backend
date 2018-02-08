c6861b11d70d883706f02699b095d472f4c78860
dilipMaharjan <dilipmaharjann@gmail.com>
2018-02-08 10:13:13 -0500

    settings syntax issue

--- tRecorderApi/tRecorderApi/settings.py
+++ tRecorderApi/tRecorderApi/settings.py
@@ -24,7 +24,8 @@ SECRET_KEY = '&9e^=922_&wi-bw@bbe$id#r$7hb(im03nrow5w@tgg8##hfd('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.96.21', '172.19.145.91', '172.19.145.88', 'localhost', '127.0.0.1', 'te.loc']
+ALLOWED_HOSTS = ['192.168.96.21', '172.19.145.91',
+                 '172.19.145.88', 'localhost', '127.0.0.1', 'te.loc']
 
 # Application definition
 
@@ -90,9 +91,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
-	'USER': 'postgres',
-	'HOST': 'db',
-	'PORT': 5432,
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
@@ -140,7 +141,7 @@ REACT_APP_DIR = os.path.join(BASE_DIR, 'frontend')
 
 STATICFILES_DIRS = [
     os.path.join(REACT_APP_DIR, 'build', 'static'),
-
+]
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 SECURE_SSL_REDIRECT = False
