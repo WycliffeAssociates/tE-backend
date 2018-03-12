eea15b1b9e08385ada8fb201f22bccd5181db530
dilip <dilipmaharjann@gmail.com>
2018-03-12 11:40:10 -0400

    making api docs visible without authentication

--- tRecorderApi/tRecorderApi/urls.py
+++ tRecorderApi/tRecorderApi/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework import permissions
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -25,7 +26,8 @@ schema_view = get_schema_view(
         default_version='v1',
         description="Test description",
     ),
-    public=False,
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
