b1d18af5a92b0b65a6c7d410f861a22d54167762
vmastteam <murmax82@gmail.com>
2018-03-26 16:02:39 -0400

    removed unneccesary allowed hosts

--- tRecorderApi/tRecorderApi/settings.py
+++ tRecorderApi/tRecorderApi/settings.py
@@ -24,8 +24,7 @@ SECRET_KEY = '&9e^=922_&wi-bw@bbe$id#r$7hb(im03nrow5w@tgg8##hfd('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.96.21', '172.19.145.91',
-                 '172.19.145.88', 'localhost', '127.0.0.1', 'te.loc']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'te.loc']
 
 # Application definition
 
