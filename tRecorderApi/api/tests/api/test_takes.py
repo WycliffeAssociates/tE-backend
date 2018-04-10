b5f37682285b5f1314777a30c8451a0465490fc7
Pinto18 <pinto18.nd@gmail.com>
2018-04-10 14:34:19 -0400

    Reformatted api/test_takes.py to conform to pylint style guide

--- tRecorderApi/api/tests/api/test_takes.py
+++ tRecorderApi/api/tests/api/test_takes.py
@@ -1,14 +1,22 @@
+"""
+    This module contains test cases which test the Take objects interactions
+    with the API. For more information for how the Take object is defined,
+    please read the Take object class definition in models/take.py.
+"""
+import random
+import string
 from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APIClient
-import random
-import string
 
 from ...models import Language, Anthology, Book, Version, Mode, Project, Chapter, Chunk, Take
 
 
 class TakesApiTest(TestCase):
-
+    """
+        This test case contains tests which verify the behavior of the API as it
+        interacts with the Take object class.
+    """
     def setUp(self):
         self.client = APIClient()
         self.lang = Language.objects.create(
@@ -55,41 +63,80 @@ class TakesApiTest(TestCase):
             string.ascii_uppercase + string.digits,
             k=random.randint(1, 15)))
 
-    def test_number_of_items_are_equal(self):
-        language_num = Take.objects.count()
-        response = self.client.get('/api/takes/')
-        self.assertEqual(len(response.data), language_num)
-
-    def test_get_request_has_200_status_code(self):
+    def test_get_request_returns_ok(self):
+        """
+            Verify send a GET request to localhost:8000/api/takes/ sends an
+            HTTP Response back with a status code of 200.
+            Input:    GET request to localhost:8000/api/takes/
+            Expected: HTTP response returned with status code 200
+        """
         response = self.client.get('/api/takes/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_get_request_with_id_has_200_status_code(self):
-        response = self.client.get('/api/takes/1/')
+    def test_get_request_with_id(self):
+        """
+            Verify an object that has a record inside the DB can be accessed in
+            the API using a URL containing its respective id.
+            Input:    GET request to 'localhost:8000/api/takes/' with a parameter
+                        containing the take object's id appended to it.
+            Expected: API will return an HTTP response with status code 200
+
+        """
+        response = self.client.get('/api/takes/' + str(self.take.id) + '/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        response = self.client.get('/api/takes/?id='+str(self.take.id))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    # def test_get_request_with_id_has_200_status_code(self):
-        # response = self.client.get('/api/takes/?id=1')
-        # self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_get_request_with_non_existent_id_has_404_status_code(self):
-        response = self.client.get('/api/takes/4/')
+    def test_null_get_request(self):
+        """
+            Verify sending a GET request to the API with an id parameter for a
+            Take object that does not exist within the DB will return an HTTP
+            response with a status code of 404.
+            Input:    Get request to localhost:8000/api/takes/1000
+            Expected: HTTP response will be returned with status code 404
+        """
+        response = self.client.get('/api/takes/1000/')
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-    def test_random_text_as_parameter_gives_400_status_code(self):
-        response = self.client.get('/api/takes/?'+self.random_url)
+    def test_random_parameter_in_url(self):
+        """
+            Verify sending a GET request for a take object using a parameter in
+            the URL that does not exist will return a bad request response
+            (HTTP response with status code 400).
+            Input:    GET request to 'localhost:8000/api/takes/' with a random string
+                        as an additional parameter
+            Expected: The API will return with an HTTP response with a status
+                        code of 400.
+        """
+        response = self.client.get('/api/takes/?' + self.random_url)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_rating_is_updated(self):
-        self.client.patch('/api/takes/1/', {"rating": 1})
-        take = Take.objects.get(id=1)
+        """
+            Verify a Take object can be updated using the API.
+            Input:    PUT request to API URL that points to the Take object
+                        created in the setup method with a change to one of its
+                        attributes.
+            Expected: The Take object will be updated with its new data.
+        """
+        self.client.patch('/api/takes/' + str(self.take.id) +'/', {"rating": 1})
+        take = Take.objects.get(id=self.take.id)
         self.assertEqual(take.rating, 1)
 
-    def test_published_is_updated(self):
-        self.client.patch('/api/takes/1/', {"published": False})
-        take = Take.objects.get(id=1)
-        self.assertFalse(take.published)
-    
+    # TODO: The test below performs a similar test to the one above. Please review
+    # whether this test should be removed due to redundancy.
+    # def test_published_is_updated(self):
+        # """
+            # Verify a Take object can be updated using the API.
+            # Input:    PUT request to API URL that points to the Take object
+                        # created in the setup method with a change to one of its
+                        # attributes.
+            # Expected: The Take object will be updated with its new data.
+        # """
+        # self.client.patch('/api/takes/' + str(self.take.id) + '/', {"published": False})
+        # take = Take.objects.get(id=self.take.id)
+        # self.assertFalse(take.published)
+
     def tearDown(self):
         self.lang.delete()
         self.anthology.delete()
