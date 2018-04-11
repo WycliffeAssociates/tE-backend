23629af4e13f3b2b1005093505f65bc158b063c3
Pinto18 <pinto18.nd@gmail.com>
2018-04-11 16:38:55 -0400

    Added expose argument in docker-compose file so containers can communicate on jenkins server

--- tRecorderApi/tRecorderApi/settings_test.py
+++ tRecorderApi/tRecorderApi/settings_test.py
@@ -24,7 +24,7 @@ SECRET_KEY = '&9e^=922_&wi-bw@bbe$id#r$7hb(im03nrow5w@tgg8##hfd('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost']
 
 # Application definition
 
