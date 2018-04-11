4ca19a79cdd444bcd4ff14ece16d746858d84eba
Pinto18 <pinto18.nd@gmail.com>
2018-04-11 14:06:51 -0400

    Fixed migration issue in unit test docker container

--- tRecorderApi/api/tests/unit_test_container/config/init-user-sb.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-	GRANT ALL PRIVILEGES ON DATABASE postgres TO postgres;
-EOSQL
--- tRecorderApi/api/tests/unit_test_container/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-cd /tE-backend/tRecorderApi
-python3 manage.py makemigrations --settings-tRecorderApi.settings_test
-python3 manage.py migrate --settings=tRecorderApi.settings_test
-python3 manage.py jenkins --enable-coverage --settings=tRecorderApi.settings_test
