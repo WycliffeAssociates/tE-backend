fdf6abbe0c1c26cc35610d48fd22d38d42171246
vmastteam <murmax82@gmail.com>
2018-04-12 14:50:32 -0400

    deleted old code

--- tRecorderApi/api/views/clean_orphans.py
+++ tRecorderApi/api/views/clean_orphans.py
@@ -3,10 +3,9 @@ import datetime
 from django.http import JsonResponse
 
 from api.file_transfer import FileUtility
-from api.tasks import cleanup_orphan_files, test_task
+from api.tasks import cleanup_orphan_files
 
 
 def clean_orphans(request):
     task = cleanup_orphan_files.delay(FileUtility(), title='Clean orphan files', started=datetime.datetime.now())
-    # task = test_task.delay(title='Test task', started=datetime.datetime.now())
     return JsonResponse({"response": "processing", "task_id": task.id})
