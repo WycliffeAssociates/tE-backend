49911704b994f5b5fc730a72eaf55ddb320737f5
vmastteam <murmax82@gmail.com>
2018-04-13 16:41:25 -0400

    refactored user patch method to support updating all the properties

--- tRecorderApi/api/pipeline.py
+++ tRecorderApi/api/pipeline.py
@@ -2,11 +2,9 @@ from django.contrib.auth.hashers import make_password
 
 
 def user_created(strategy, details, user=None, *args, **kwargs):
-    print(user.username)
-
     if user:
-        # password = make_password("some_password")
+        password = make_password("P@ssw0rd")
 
-        user.is_staff = True
-        # user.password = password
+        user.is_social = True
+        user.password = password
         user.save()
