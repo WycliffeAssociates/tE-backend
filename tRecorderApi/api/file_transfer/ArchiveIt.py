5f81919df96cf353ec9ff6f0210977c8a90f65d2
vmastteam <murmax82@gmail.com>
2018-04-17 11:39:22 -0400

    Refactored some functions in tasks manager

--- tRecorderApi/api/file_transfer/ArchiveIt.py
+++ tRecorderApi/api/file_transfer/ArchiveIt.py
@@ -3,26 +3,26 @@ from .ArchiveProject import ArchiveProject
 
 
 class ArchiveIt(ArchiveProject):
-    def archive(self, root_dir, project_file, location_list, remove_dir, task, title, started):
+    def archive(self, root_dir, project_file, location_list, remove_dir, project, update_progress, task_args):
         with zipfile.ZipFile(project_file, 'w') as zipped_f:
             current_take = 0
             for file in location_list:
                 zipped_f.write(file["conv"], file["conv"].replace(root_dir, ""))
 
                 current_take += 1
-                progress = int(((current_take / len(location_list) * 100) / 3) + ((100 / 3) * 2))  # 3/3 of overall task
-                task.update_state(state='PROGRESS',
-                                  meta={
-                                      'current': progress,
-                                      'total': 100,
-                                      'name': task.name,
-                                      'started': started,
-                                      'title': title,
-                                      'message': 'Zipping takes...',
-                                      'details': {
-                                          'result': file["fn"],
-                                      }
-                                  })
+
+                if project and update_progress and task_args:
+                    # 3/3 of overall task
+                    progress = int(((current_take / len(location_list) * 100) / 3) + ((100 / 3) * 2)) - 1
+
+                    new_task_args = task_args + (progress, 100, 'Zipping takes...', {
+                        'lang_slug': project["lang_slug"],
+                        'lang_name': project["lang_name"],
+                        'book_slug': project["book_slug"],
+                        'book_name': project["book_name"],
+                        'result': file["fn"]
+                    })
+                    update_progress(*new_task_args)
 
         remove_dir(root_dir)
         return project_file
