be55226ac5d173b336e232c3c71a27d451f51ffb
dilip <dilipmaharjann@gmail.com>
2018-06-08 16:25:02 -0400

    dupliate takes won't upload

--- tRecorderApi/api/file_transfer/ZipIt.py
+++ tRecorderApi/api/file_transfer/ZipIt.py
@@ -19,35 +19,37 @@ class ZipIt(ArchiveProject):
             with zipfile.ZipFile(file, "r") as zip_file:
                 takes = zip_file.infolist()
                 diff_list = ZipIt.get_diff_list(ZipIt.get_files(zip_file), zip_file)
-                for d in diff_list:
-                    print(d)
+                keys = set().union(*(d.keys() for d in diff_list))
                 current_take = 0
                 for i, take in enumerate(takes):
                     filename = take.filename
-                    if len(filename) == 12:
-                        zip_file.extract(take, os.path.join(os.path.dirname(directory), "name_audios"))
-                        continue
-                    if len(filename) > 12 and filename.endswith(".mp3"):
-                        zip_file.extract(take, os.path.join(os.path.dirname(directory), "comments"))
-                        continue
-                    zip_file.extract(take, directory)
+                    if filename in keys:
+                        if len(filename) == 12:
+                            loc = os.path.join(os.path.dirname(directory), "name_audios")
+                            zip_file.extract(take, loc)
+                            continue
+                        if len(filename) > 12 and filename.endswith(".mp3"):
+                            loc = os.path.join(os.path.dirname(directory), "comments")
+                            zip_file.extract(take, loc)
+                            continue
+                        zip_file.extract(take, directory)
 
-                    current_take += 1
+                        current_take += 1
 
-                    if update_progress and task_args:
-                        # 1/2 of overall task
-                        progress = int(((current_take / len(takes) * 100) / 2))
+                        if update_progress and task_args:
+                            # 1/2 of overall task
+                            progress = int(((current_take / len(takes) * 100) / 2))
 
-                        new_task_args = task_args + (progress, 100, 'Extracting takes...', {
-                            'user_icon_hash': user["icon_hash"],
-                            'user_name_audio': user["name_audio"],
-                            'lang_slug': "--",
-                            'lang_name': "--",
-                            'book_slug': "--",
-                            'book_name': "--",
-                            'result': str(take.filename)
-                        })
-                        update_progress(*new_task_args)
+                            new_task_args = task_args + (progress, 100, 'Extracting takes...', {
+                                'user_icon_hash': user["icon_hash"],
+                                'user_name_audio': user["name_audio"],
+                                'lang_slug': "--",
+                                'lang_name': "--",
+                                'book_slug': "--",
+                                'book_name': "--",
+                                'result': str(take.filename)
+                            })
+                            update_progress(*new_task_args)
 
             return 'ok', 200
 
