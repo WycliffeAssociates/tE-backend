7831a6cd79ddc65251033267e02df17e9f75442c
dilip <dilipmaharjann@gmail.com>
2018-06-12 13:46:13 -0400

    uploads only new files

--- tRecorderApi/api/file_transfer/ZipIt.py
+++ tRecorderApi/api/file_transfer/ZipIt.py
@@ -5,6 +5,8 @@ import zipfile
 
 from api.models import Take
 
+from api.models import Comment
+from api.models.take import User
 from .ArchiveProject import ArchiveProject
 
 
@@ -17,42 +19,50 @@ class ZipIt(ArchiveProject):
     def extract(file, directory, user, update_progress, task_args):
         try:
             with zipfile.ZipFile(file, "r") as zip_file:
-                takes = zip_file.infolist()
-                diff_list = ZipIt.get_diff_list(ZipIt.get_files(zip_file), zip_file)
-                keys = set().union(*(d.keys() for d in diff_list))
+                takes_info = zip_file.infolist()
+                takes = ZipIt.get_files(zip_file)
+                keys = None
+                if takes:
+                    print("In takes")
+                    user_comment = ZipIt.get_users_comments(takes)
+                    diff_list = ZipIt.get_diff_list(takes, zip_file, user_comment)
+                    keys = set().union(*(d.keys() for d in diff_list))
+
                 current_take = 0
-                for i, take in enumerate(takes):
+                for i, take in enumerate(takes_info):
                     filename = take.filename
-                    if filename in keys:
-                        if len(filename) == 12:
-                            loc = os.path.join(os.path.dirname(directory), "name_audios")
-                            zip_file.extract(take, loc)
+                    if keys is not None:
+                        if filename not in keys:
                             continue
-                        if len(filename) > 12 and filename.endswith(".mp3"):
-                            loc = os.path.join(os.path.dirname(directory), "comments")
-                            zip_file.extract(take, loc)
-                            continue
-                        zip_file.extract(take, directory)
 
-                        current_take += 1
+                    if len(filename) == 12:
+                        loc = os.path.join(os.path.dirname(directory), "name_audios")
+                        zip_file.extract(take, loc)
+                        continue
+                    if len(filename) > 12 and filename.endswith(".mp3"):
+                        loc = os.path.join(os.path.dirname(directory), "comments")
+                        zip_file.extract(take, loc)
+                        continue
+                    zip_file.extract(take, directory)
 
-                        if update_progress and task_args:
-                            # 1/2 of overall task
-                            progress = int(((current_take / len(takes) * 100) / 2))
+                current_take += 1
 
-                            new_task_args = task_args + (progress, 100, 'Extracting takes...', {
-                                'user_icon_hash': user["icon_hash"],
-                                'user_name_audio': user["name_audio"],
-                                'lang_slug': "--",
-                                'lang_name': "--",
-                                'book_slug': "--",
-                                'book_name': "--",
-                                'result': str(take.filename)
-                            })
-                            update_progress(*new_task_args)
+                if update_progress and task_args:
+                    # 1/2 of overall task
+                    progress = int(((current_take / len(takes_info) * 100) / 2))
+
+                    new_task_args = task_args + (progress, 100, 'Extracting takes...', {
+                        'user_icon_hash': user["icon_hash"],
+                        'user_name_audio': user["name_audio"],
+                        'lang_slug': "--",
+                        'lang_name': "--",
+                        'book_slug': "--",
+                        'book_name': "--",
+                        'result': str(take.filename)
+                    })
+                    update_progress(*new_task_args)
 
             return 'ok', 200
-
         except zipfile.BadZipfile as e:
             return e, 400
 
@@ -76,6 +86,18 @@ class ZipIt(ArchiveProject):
             chunk__chapter__project__anthology__slug__iexact=anthology).filter(
             chunk__chapter__project__version__slug__iexact=version).filter(
             chunk__chapter__project__book__slug__iexact=book)
+
+    @staticmethod
+    def get_users_comments(takes):
+        comment_hash = []
+        user_hash = []
+        for take in takes:
+            comment = Comment.get_comments(take_id=take.id)
+            if comment:
+                user = User.objects.filter(comment=comment)
+                user_hash.append({user.name_audio, ZipIt.get_local_file_hash(user.location)})
+                comment_hash.append({take.name, ZipIt.get_local_file_hash(comment.location)})
+        return comment_hash + user_hash
 
     @staticmethod
     def get_local_file_hash(location):
@@ -108,7 +130,7 @@ class ZipIt(ArchiveProject):
         return hashes
 
     @staticmethod
-    def get_diff_list(local_file_list, zip_file):
-        local_list = ZipIt.local_file_hash_list(local_file_list)
+    def get_diff_list(local_file_list, zip_file, user_comment):
+        local_list = ZipIt.local_file_hash_list(local_file_list) + user_comment
         zip_list = ZipIt.zip_file_hash_list(zip_file)
         return [x for x in local_list + zip_list if x not in local_list or x not in zip_list]
