46d7a65c5994afd20349452323eb3adbd8520312
Pinto18 <pinto18.nd@gmail.com>
2018-07-05 10:07:29 -0400

    Started writiing the unit test for FileUtility.get_markers()

--- tRecorderApi/api/tests/test_file_transfer.py
+++ tRecorderApi/api/tests/test_file_transfer.py
@@ -15,8 +15,9 @@ class FileUtilityTest(TestCase):
     """
     # This would be a mocked out version of the update progress function, which
     # is alright since we are not focusing on testing this function here.
-    # def mock_update_progress(self, task):
-        # return True
+    def mock_progress(self, task):
+        print("running mock progress.\n")
+        return True
 
     @patch('api.models.language.Language')
     @patch('api.models.anthology.Anthology')
@@ -29,9 +30,9 @@ class FileUtilityTest(TestCase):
     @patch('api.models.take.Take')
     @patch('api.models.comment.Comment')
     @patch('api.file_transfer.tinytag.TinyTag')
-    @patch('api.tasks.update_progress')
+    @patch('api.tasks.update_progress', side_effect=mock_progress)
     @patch('api.file_transfer.FileUtility')
-    @patch('api.file_transfer.FileUtility.os.path')
+    @patch('api.file_transfer.os.path')
     def test_import_project(
             self,
             mock_path,
@@ -58,85 +59,85 @@ class FileUtilityTest(TestCase):
         # Build Dependencies; in its entirety we are mocking a project for the
         #                     book of Joshua
         # Language
-        mock_language.slug.return_value = 'en'
-        mock_language.name.return_value = 'English'
+        mock_language.slug = 'en'
+        mock_language.name = 'English'
         mock_language.import_language.return_value = mock_language
 
         #Antholgoy
-        mock_anthology.slug.return_value = 'ot'
-        mock_anthology.name.return_value = 'old testament'
+        mock_anthology.slug = 'ot'
+        mock_anthology.name = 'old testament'
         mock_anthology.import_anthology.return_value = mock_anthology
 
         # Book
-        mock_book.anthology.return_value = mock_anthology
-        mock_book.slug.return_value = 'jos'
-        mock_book.name.return_value = 'Joshua'
-        mock_book.number.return_value = 6
+        mock_book.anthology = mock_anthology
+        mock_book.slug = 'jos'
+        mock_book.name = 'Joshua'
+        mock_book.number = 6
         mock_book.import_book.return_value = mock_book
 
         # Version
-        mock_version.slug.return_value = 'ulb'
-        mock_version.name.return_value = 'unlocked literal bible'
+        mock_version.slug = 'ulb'
+        mock_version.name = 'unlocked literal bible'
         mock_version.import_version.return_value = mock_version
 
         # Mode
-        mock_mode.slug.return_value = 'chunk'
-        mock_mode.name.return_value = 'chunk'
-        mock_mode.unit.return_value = 'MULTI'
+        mock_mode.slug = 'chunk'
+        mock_mode.name = 'chunk'
+        mock_mode.unit = 'MULTI'
         mock_mode.import_mode.return_value = mock_mode
 
         #Project
-        mock_project.id.return_value = 1
-        mock_project.version.return_value = mock_version
-        mock_project.mode.return_value = mock_mode
-        mock_project.anthology.return_value = mock_anthology
-        mock_project.language.return_value = mock_language
-        mock_project.book.return_value = mock_book
-        mock_project.published.return_value = False
+        mock_project.id = 1
+        mock_project.version = mock_version
+        mock_project.mode = mock_mode
+        mock_project.anthology = mock_anthology
+        mock_project.language = mock_language
+        mock_project.book = mock_book
+        mock_project.published = False
         mock_project.import_project.return_value = mock_project
 
         # Chapter
-        mock_chapter.number.return_value = 1
-        mock_chapter.checked_level.return_value = 0
-        mock_chapter.published.return_value = False
-        mock_chapter.project.return_value = mock_project
-        mock_chapter.comments.return_value = None
+        mock_chapter.number = 1
+        mock_chapter.checked_level = 0
+        mock_chapter.published = False
+        mock_chapter.project = mock_project
+        mock_chapter.comments = None
         mock_chapter.import_chapter.return_value = mock_chapter
 
         # Chunk
-        mock_chunk.startv.return_value = 1
-        mock_chunk.endv.return_value = 3
-        mock_chunk.chapter.return_value = mock_chapter
-        mock_chunk.comments.return_value = None
+        mock_chunk.startv = 1
+        mock_chunk.endv = 3
+        mock_chunk.chapter = mock_chapter
+        mock_chunk.comments = None
         mock_chunk.import_chunk.return_value = mock_chunk
 
         # Take
-        mock_take.location.return_value = 'some path'
-        mock_take.duration.return_value = 0
-        mock_take.rating.return_value = 0
-        mock_take.published.return_value = False
-        mock_take.markers.return_value = ''
-        mock_take.date_modified.return_value = '2018-07-02 1:53'
-        mock_take.chunk.return_value = mock_chunk
-        mock_take.comments.return_value = None
-        mock_take.owner.return_value = None  #no user
+        mock_take.location = 'some path'
+        mock_take.duration = 0
+        mock_take.rating = 0
+        mock_take.published = False
+        mock_take.markers = ''
+        mock_take.date_modified = '2018-07-02 1:53'
+        mock_take.chunk = mock_chunk
+        mock_take.comments = None
+        mock_take.owner = None  #no user
         mock_take.import_take.return_value = mock_take
 
         # Comment
-        mock_comment.location.return_value = "some path"
-        mock_comment.date_modified.return_value = "2018-07-02 2:33"
+        mock_comment.location = "some path"
+        mock_comment.date_modified = "2018-07-02 2:33"
         mock_comment.import_comment.return_value = True
 
         # TinyTag
-        mock_tiny_tag.filehandler.return_value = None
-        mock_tiny_tag.filsize.return_value = 0
+        mock_tiny_tag.filehandler = None
+        mock_tiny_tag.filsize = 0
         mock_tiny_tag.get.return_value = mock_tiny_tag
 
         # tasks.update_progress
-        mock_progress.return_value = True
+        # mock_progress.return_value = True
 
         # FileUtility
-        mock_file_utility.open_manifest.return_value = None
+        mock_file_utility.open_manifest_file.return_value = None
         mock_file_utility.manifest_takes_count.return_value = 2
         mock_file_utility.get_markers.return_value = 0
 
@@ -156,11 +157,13 @@ class FileUtilityTest(TestCase):
             'message' : 'these are fake arguments'
         }
 
+        print("Calculating actual value.\n")
         actual = test_file_utility.import_project(
             directory=test_directory,
             user=test_user,
             update_progress=mock_progress,
             task_args=test_args)
+        print("Calculated actual value.\n")
         expected = {
             'user_icon_hash'  : 'fake hash',
             'user_name_audio' : 'fake_audio',
